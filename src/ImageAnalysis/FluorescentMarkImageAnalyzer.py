9d04d99720cb3738493f52d8beada7902f161775
Blanka Chamrádová <chamradova.bt@gmail.com>
2022-10-25 16:33:44 +0200

    added wider fluorescent mark detection, added options for surroundings to imageAnalyzer

--- src/ImageAnalysis/FluorescentMarkImageAnalyzer.py
+++ src/ImageAnalysis/FluorescentMarkImageAnalyzer.py
@@ -2,6 +2,8 @@ import sys
 
 from PIL import Image
 import numpy as np
+import scipy.ndimage
+from src.Helpers.MatrixHelper import MatrixHelper
 from src.Model.Molecule import Molecule
 from src.Model.FluorescentMark import FluorescentMark
 from src.ImageAnalysis.NoiseAnalyzer import NoiseAnalyzer
@@ -18,10 +20,10 @@ class FluorescentMarkImageAnalyzer:
 
     def getPixelValue(self, x: int, y: int) -> int:
         return self.image.getpixel((x, y))
-
-    def getSurroundingValues(self, x: int, y: int, width: int):
+    #shape s=square c=circle
+    def getSurroundingValues(self, x: int, y: int, width: int, shape='s'):
         matrixWidth = 2 * width + 1
-        surroundings = [[0 for _ in range(matrixWidth)] for _ in range(matrixWidth)]
+        surroundings = np.zeros((matrixWidth, matrixWidth))
         startingX = x - width
         startingY = y - width
         for i in range(matrixWidth):
@@ -30,11 +32,14 @@ class FluorescentMarkImageAnalyzer:
                     surroundings[i][j] = self.getPixelValue(startingX + j, startingY + i)
                 except IndexError:
                     surroundings[i][j] = 0
+        if shape== 's':
+            return surroundings
+        else:
+            return MatrixHelper.getCircularValuesFromMatrix(surroundings)
 
-        return surroundings
 
-    def getFluorescentMarkSurroundingValues(self, fluorescentMark: FluorescentMark, width: int = 1):
-        return self.getSurroundingValues(fluorescentMark.posX, fluorescentMark.posY, width)
+    def getFluorescentMarkSurroundingValues(self, fluorescentMark: FluorescentMark, width: int = 1, shape='s'):
+        return self.getSurroundingValues(fluorescentMark.posX, fluorescentMark.posY, width, shape)
 
     def getPositionedFluorescentMarkValues(self, molecule: Molecule):
         marks = []
@@ -69,9 +74,11 @@ class FluorescentMarkImageAnalyzer:
     def getPixelValuesOnMoleculeLine(self, molecule):
         coordinatesOnLine = molecule.lineEquation.getPointsFromStartToEnd()
         pixelValuesOnLine = []
+        positions = []
         for point in coordinatesOnLine:
             pixelValuesOnLine.append(self.getPixelValue(point[0], point[1]))
-        return pixelValuesOnLine
+            positions.append((point[0], point[1]))
+        return pixelValuesOnLine, positions
 
     def getInterpolatedPixelValuesOnMoleculeLine(self, molecule):
         coordinatesOnLine = molecule.lineEquation.getPointsFromStartToEnd()
@@ -82,4 +89,31 @@ class FluorescentMarkImageAnalyzer:
             curveValues.append(mean)
 
         return curveValues
+
+    def getPotentialMarksOnMolecule(self, molecule, lowerBound=0, border = 0):
+        width = abs(molecule.endX - molecule.startX) + 1 + 2*border
+        height = abs(molecule.totalEndY - molecule.totalStartY) + 1 + 2*border
+        startX = molecule.startX if molecule.startX < molecule.endX else molecule.endX - border
+        endX = molecule.endX if molecule.startX < molecule.endX else molecule.startX
+
+        moleculeCutout = np.zeros((height, width), dtype=np.uint64)
+        moleculePositions = np.zeros((height, width), dtype='i,i')
+        for x in range(width):
+            for y in range(height):
+                posX = startX + x
+                posY = molecule.totalStartY + y
+                if posX >= 1042 or posY >= 8192:
+                    continue
+                moleculeCutout[y][x] = self.getPixelValue(posX, posY)
+                moleculePositions[y][x] = (posX, posY)
+        np.set_printoptions(threshold=sys.maxsize)
+        mx = scipy.ndimage.maximum_filter(moleculeCutout, size=(width*2+1, 3))
+        #todo same line one value
+        marksFiltered = np.where((mx == moleculeCutout) & (mx>lowerBound), moleculeCutout, 0)
+        nonZero = list(zip(*np.nonzero(marksFiltered)))
+
+        positions = np.array([moleculePositions[pos[0]][pos[1]] for pos in nonZero])
+        marks = np.array([marksFiltered[pos[0]][pos[1]] for pos in nonZero])
+
+        return marks, positions
 
